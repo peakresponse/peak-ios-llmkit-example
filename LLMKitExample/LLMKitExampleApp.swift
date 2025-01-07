c8030b55d18e9062bf27010853acfdd5135d8e97
Francis Li <mail@francisli.com>
2025-01-07 14:18:58 -0800

    Initial Commit

---
+++ LLMKitExample/LLMKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMKitExampleApp.swift
+//  LLMKitExample
+//
+//  Created by Francis Li on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LLMKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LLMKitExampleTests/LLMKitExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LLMKitExampleTests.swift
+//  LLMKitExampleTests
+//
+//  Created by Francis Li on 1/7/25.
+//
+
+import Testing
+@testable import LLMKitExample
+
+struct LLMKitExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
